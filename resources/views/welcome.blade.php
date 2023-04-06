fa49a1bb3571b75b4169a3e04fef1c603bff1e06
fadlunmadihij <fadlunmadihij@gmail.com>
2023-04-06 16:52:38 +0700

    p

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <html lang="en">
 
 <head>
- {{-- tanda --}}
+ {{-- tanda tanda jdfhsdjfhsdjfh --}}
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
