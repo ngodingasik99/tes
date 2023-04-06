0a97ccc6689447065e0d92d30b18107fcc632cf1
fadlunmadihij <fadlunmadihij@gmail.com>
2023-04-06 19:31:48 +0700

    navbar

--- resources/views/kategori/index.blade.php
+++ resources/views/kategori/index.blade.php
@@ -11,7 +11,7 @@
     <title>Hello, world!</title>
   </head>
   <body>
-    
+    {{-- menambahkan navbar --}}
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">Navbar</a>
