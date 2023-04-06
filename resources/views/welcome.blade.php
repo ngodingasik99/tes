10e548572090a7bdf6f47d3074ecb1893aa944f9
fadlunmadihij <fadlunmadihij@gmail.com>
2023-04-06 16:20:58 +0700

    ubah view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Aplikasi Pos</title>
+    <title>Aplikasi Pos</title> 
 
     <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
